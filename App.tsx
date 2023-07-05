5f443e0c023609d02d15e5a69434c6c50b1dd0bc
Éric  António <ericantoniodev@gmail.com>
2023-07-05 19:16:53 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Dashboard } from './src/screens/Dasboard/Dashboard';
+
+
 export default function App() {
-  return (
-    <Dashboard/>
-  );
+  return <Dashboard/>
 }
 
