89706ecda3312caee4856047c297d2fd0990b93c
Éric  António <ericantoniodev@gmail.com>
2023-07-05 19:03:10 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Welcomme } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dasboard/Dashboard';
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-    </View>
+    <Dashboard/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
