7f93fd760be1a11c60752438ea8bc29db4e62dee
Éric  António <ericantoniodev@gmail.com>
2023-07-06 12:49:24 +0200

    Theme aplyed!

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,15 +1,20 @@
 export default {
   colors: {
     primary: "#5636D3",
+    
     secondary: "#FF872C",
     secondary_ligth: "rgba(255,135,44,0.3)",
+
     success: "#12A454",
     success_ligth: "rgba( 18, 164 , 84 ,0.5)",
+
     attention: "#E83F5B",
     attention_ligth: "rgba(232, 63, 91, 0.5)",
+
+
     shape: "#FFFFFF",
-    title: "#969CB2",
+    title: "#363F5F",
     text: "#969CB2",
-    backroug: "#F0F2F5",
+    background: "#F0F2F5",
   },
 };
--- src/screens/Dasboard/styles.ts
+++ src/screens/Dasboard/styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components/native";
+import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: whitesmoke;
+  background-color: ${({theme}) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
-  font-size: 18px;
+  font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${({theme}) => theme.colors.title};
 `;
