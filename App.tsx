02b0ae0b067d3dfa4c5a8cf816ccfb7bd905a4dd
Éric  António <ericantoniodev@gmail.com>
2023-07-07 19:47:10 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-
+import SplashScreen from 'expo-splash-screen';
+import AppLoading from 'expo-app-loading';
 import {
   useFonts,
-  
-
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
@@ -13,6 +15,15 @@ import { Dashboard } from './src/screens/Dasboard';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+  
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if(!fontsLoaded){
+    return SplashScreen;
+  }
   return (
     <ThemeProvider theme={theme}>
       <Dashboard/>
@@ -20,4 +31,5 @@ export default function App() {
     
   )
 }
+
 
