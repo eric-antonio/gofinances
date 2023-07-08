c9ad183a811715cb4a0efb80461326930e4205d1
Éric  António <ericantoniodev@gmail.com>
2023-07-08 19:14:20 +0200

    Greeting Done

--- src/screens/Dasboard/styles.ts
+++ src/screens/Dasboard/styles.ts
@@ -33,6 +33,12 @@ export const User= styled.View`
   margin-left:17px ;
 `;
 
-export const UserGreeting= styled.Text``;
+export const UserGreeting= styled.Text`
+  color: ${({theme})=> theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
+`;
 
-export const UserName= styled.Text``;
+export const UserName= styled.Text`
+  
+`;
