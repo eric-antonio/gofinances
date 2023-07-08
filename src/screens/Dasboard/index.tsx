28386028b891ccb29ec65cfcbfd70aef53d90639
Éric  António <ericantoniodev@gmail.com>
2023-07-08 19:05:45 +0200

    Rf parcentege

--- src/screens/Dasboard/index.tsx
+++ src/screens/Dasboard/index.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { 
   Container,
-  Header
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+
 } from "./styles";
+
 
 export function Dashboard(){
   return(
     <Container>
       <Header>
-        
+
+        <UserInfo>
+
+          <Photo source = {{uri:'https://github.com/eric-antonio.png'}}/>
+
+          <User>
+
+            <UserGreeting>Olá,</UserGreeting>
+            <UserName>Éric Antonio </UserName>
+
+          </User>
+
+        </UserInfo>
+
       </Header>
     </Container>
   )
--- src/screens/Dasboard/styles.ts
+++ src/screens/Dasboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
 import { RFPercentage } from "react-native-responsive-fontsize";
+import { View } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
@@ -8,8 +9,22 @@ export const Container = styled.View`
 `;
 
 export const Header =  styled.View`
-
+  justify-content: center;
+  align-items: center;
   width: 100%;
+  flex-direction: row;
   height: ${RFPercentage(42)}px;
   background-color: ${({theme}) => theme.colors.primary};
 `;
+
+export const UserInfo = styled.View`
+
+`;
+
+export const Photo= styled.Image``;
+
+export const User= styled.View``;
+
+export const UserGreeting= styled.Text``;
+
+export const UserName= styled.Text``;
