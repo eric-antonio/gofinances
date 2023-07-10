9bf795581cf3cc7f80ac3ce957199b075c5bf546
Éric  António <ericantoniodev@gmail.com>
2023-07-10 06:47:44 +0200

    Starting the card Section

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Container } from "./styles";
 
-
-export function Card(){
+export function HighlightCard(){
   return(
-    
+    <Container>
+
+    </Container>
   )
 }
