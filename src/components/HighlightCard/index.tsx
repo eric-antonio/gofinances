bdeb7b21617182818fe98513acf71695e0ef9bc8
Éric  António <ericantoniodev@gmail.com>
2023-07-10 17:53:54 +0200

    Header

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -21,15 +21,25 @@ const icon = {
 }
 export function HighlightCard({title,amount,lastTransaction,type}:Props){
   return(
-    <Container>
+    <Container type={type}>
 
       <Header>
-        <Title> {title} </Title>
-        <Icon name={icon[type]}/>
+
+        <Title type={type} > {title} </Title>
+
+        <Icon name={icon[type]} type={type}/>
+
       </Header>
+
       <Footer>
-        <Amount>{amount}</Amount>
-        <LastTransaction>{lastTransaction}</LastTransaction>
+
+        <Amount type={type} >
+          {amount}
+        </Amount>
+        <LastTransaction type={type}>
+          {lastTransaction}
+        </LastTransaction>
+
       </Footer>
 
     </Container>
