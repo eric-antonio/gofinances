cf0dbe4cc2ca240d755281e17414fce557b1b1c7
Éric  António <ericantoniodev@gmail.com>
2023-07-14 22:06:19 +0200

    Update index.tsx

--- src/screens/Dasboard/index.tsx
+++ src/screens/Dasboard/index.tsx
@@ -118,14 +118,7 @@ export function Dashboard(){
       </HighlightCards>
 
 
-      <Transactions>
-        <Title>Listagem</Title>
-        <TransactionList
-          data={data}
-          keyExtractor = { item => item.id}
-          renderItem= {({item}) => <TransactionCard data={item}/> }
 
-        />
       </Transactions>
      
     </Container>
