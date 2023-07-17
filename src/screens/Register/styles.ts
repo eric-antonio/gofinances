830980ebfc1b4a345ea041f5041d51bb911c799c
Éric  António <ericantoniodev@gmail.com>
2023-07-17 14:51:26 +0200

    Creating component textInput

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -1,8 +1,22 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
 
 `;
 
-export const Header = styled.View``;
-export const Title = styled.Text``;
+export const Header = styled.View`
+  background-color: ${({theme}) => theme.colors.primary};
+  width: 100%;
+
+  height: ${RFValue(113)}px;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
+`;
+export const Title = styled.Text`
+  font-size:18px;
+  color: ${({theme}) => theme.colors.text};
+  background-color: ${({theme}) => theme.fonts.regular};
+  
+`;
