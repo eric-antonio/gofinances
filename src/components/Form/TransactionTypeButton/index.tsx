a036a77afa3eb1792f0b4ebcfa98c00060c8d7b0
Éric  António <ericantoniodev@gmail.com>
2023-07-19 20:29:29 +0200

    Update index.tsx

--- src/components/Form/TransactionTypeButton/index.tsx
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { TouchableOpacityProps } from "react-native";
 import {
   Container,
+  Icon,
+  Title
 }from './styles';
 
-export function TransactionTypeButton(){
+interface Pros extends TouchableOpacityProps{
+  title : string;
+  type: 'up' | 'down';
+}
+
+export function TransactionTypeButton({title, ...rest} : Pros){
   return(
     <Container>
 
+      <Icon/>
+      <Title> {title}</Title>
     </Container>
   )
 }
