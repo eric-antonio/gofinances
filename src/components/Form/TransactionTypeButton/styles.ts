ad1dc5e65183a2e79d39366c26e0d346ae6f8bec
Éric  António <ericantoniodev@gmail.com>
2023-07-20 09:26:01 +0200

    Update styles.ts

--- src/components/Form/TransactionTypeButton/styles.ts
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components/native";
 import { Feather} from '@expo/vector-icons';
 
-export const Container = styled.TouchableOpacity``;
+export const Container = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+`;
 
 export const Icon = styled(Feather)``;
 export const Title = styled.Text``;
