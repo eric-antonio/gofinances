6e10ede4125d3ab51dbb66ee33f3eff21c24d48d
Éric  António <ericantoniodev@gmail.com>
2023-07-20 10:26:03 +0200

    Update styles.ts

--- src/components/Form/TransactionTypeButton/styles.ts
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -2,6 +2,11 @@ import styled from "styled-components/native";
 import { Feather} from '@expo/vector-icons';
 import { RFValue } from "react-native-responsive-fontsize";
 
+interface IconProps{
+  type: 'up' | 'down';
+
+}
+
 export const Container = styled.TouchableOpacity`
   width: 48%;
 
@@ -15,10 +20,13 @@ export const Container = styled.TouchableOpacity`
   padding: 16px;
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather)<IconProps>`
 
   font-size: ${RFValue(24)}px;
   margin-right: 12px;
+
+  color: ${({theme,type}) => 
+  type === 'up' ? theme.colors.success : theme.colors.attention };
 `;
 
 export const Title = styled.Text`
