966c7d75eae5c0b5a52156b8702ae6a953b79a86
Éric  António <ericantoniodev@gmail.com>
2023-07-26 20:11:21 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -28,9 +28,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="light" />
-      {/* <Dashboard/> */}
-      <Register/>
+    
     </ThemeProvider>
     
   )
