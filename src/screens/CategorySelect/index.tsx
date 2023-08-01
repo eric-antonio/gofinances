4533fdad3baa88b74c502243bd5e13c037c8d1f5
Éric  António <ericantoniodev@gmail.com>
2023-08-01 11:15:44 +0200

    CategorySelect

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container} from './styles'
+
+interface Props{
+    category: string
+}
+
+export function CategorySelect() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
