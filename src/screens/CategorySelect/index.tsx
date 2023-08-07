fc4affd5a4cec62f56f55588941b945713ba6250
Éric  António <ericantoniodev@gmail.com>
2023-08-07 20:24:39 +0200

    Update index.tsx

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -40,7 +40,10 @@ export function CategorySelect(
         style={{flex:1, width:'100%'}}
         keyExtractor={(item) => item.key}
         renderItem={({item})=> (
-
+          <Category>
+            <Icon name = {item.icon}/>
+            <Name>{item.name}</Name>
+          </Category>
         )}
       />
 
