aeb48870eaf21e8afe8cffb3599dba5c33f85ec6
Éric  António <ericantoniodev@gmail.com>
2023-08-11 14:15:16 +0200

    Update app.routes.tsx

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
+import { useTheme } from "styled-components";
 
 
 export function AppRoutes() {
+
+    const theme = useTheme()
   return (
-    <Navigator>
+    <Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: theme.colors.secondary,
+            tabBarInactiveTintColor: theme.colors.text,
+            tabBarLabelPosition:'beside-icon',
+            tabBarStyle:{
+                height:88,
+                paddingVertical: Platform.OS === 'ios'? 20 : 0,
+            }
+        }}
+    >
+   
 
         <Screen 
             name="Listing" 
