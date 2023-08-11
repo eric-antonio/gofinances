d1f124585b2469cdd4c32e1c01b86d719c9467df
Éric  António <ericantoniodev@gmail.com>
2023-08-11 12:41:15 +0200

    Update app.routes.tsx

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -9,7 +9,23 @@ import { Register } from "../screens/Register";
 export function AppRoutes() {
   return (
     <Navigator>
-      <Screen name="Listing" component={Dashboard} />
+
+        <Screen 
+            name="Listing" 
+            component={Dashboard} 
+        />
+
+      <Screen 
+            name="Register" 
+            component={Register} 
+        />
+
+      <Screen 
+            name="Resume" 
+            component={Dashboard} 
+        />
+
     </Navigator>
+    
   );
 }
