03630917053dc0ac695573910f7bae1ddc0d87ad
Éric  António <ericantoniodev@gmail.com>
2023-08-11 15:09:57 +0200

    Update styles.ts

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
-import { RFPercentage, RFValue} from "react-native-responsive-fontsize";
-import { Feather } from'@expo/vector-icons';
-import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
 import { FlatList } from "react-native";
+import { Feather } from'@expo/vector-icons';
+import { RFPercentage, RFValue} from "react-native-responsive-fontsize";
+import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
+
 import {DataListProps} from '.';
 
 export const Container = styled.View`
@@ -65,10 +66,13 @@ export const UserName= styled.Text`
   font-family: ${({theme}) => theme.fonts.bold};
 `;
 
+
 export const Icon = styled(Feather)`
   color: ${({theme})=> theme.colors.secondary};
   font-size: ${RFValue(24)}px;
 `;
+
+
 
 export const HighlightCards = styled.ScrollView.attrs({
 
