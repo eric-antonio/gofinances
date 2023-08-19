33f09e86c3d793eec24a638784483cb7a3d79335
Éric  António <ericantoniodev@gmail.com>
2023-08-19 22:57:44 +0200

    Update index.tsx

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -118,7 +118,10 @@ export function Register() {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <Container>
 
-    
+        <Header>
+          <Title>Register</Title>
+        </Header>
+
         <Form>
           <Fildes>
             <InputFrom
