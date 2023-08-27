cac3fdc93e9ec669625ee55362f743233a861949
Éric  António <ericantoniodev@gmail.com>
2023-08-27 23:32:16 +0200

    Update index.tsx

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -42,8 +42,25 @@ export function Dashboard(){
       .toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
-      })
+      });
+      
+      const dateFormatted = Intl.DateTimeFormat('pt-BR',{
+        day:'2-digit',
+        month: '2-digit',
+        year: '2-digit'
+      }).format(new Date(item.date));
+
+      return{
+        id: item.id,
+        name : item.name,
+        amount,
+        type: item.type,
+        category : item.category,
+        dateFormatted,
+      }
     });
+
+    setData(transactionsFormatted);
 
   }
 
